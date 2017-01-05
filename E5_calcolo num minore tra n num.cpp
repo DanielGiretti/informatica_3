95bd06d9d9015855d3edea9add5da2f65b77ce64
mc-cavallo <danygiretti@hotmail.com>
2017-01-05 21:46:28 +0100

    Add files via upload

---
+++ E5_calcolo num minore tra n num.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<MATH.h>
+main()
+{
+	int n;
+	int num;
+	int somma;
+	int i;
+	int media;
+	printf("inserisciil la quantità di numeri");
+	scanf("%d",n);
+	i=0;
+	while(i<=n){
+		printf("inserisci un numero");
+		scanf("%d",num);
+		somma=somma+num;
+		i++;
+	}
+	media=somma/n;
+	printf("media = %d",media);
+}
---
+++ E6_fattoriale.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	int fat;
+	fat=1;
+	printf("inserisci un numero");
+	scanf("%d",n);
+	while(n>0){
+		fat=fat*n;
+		n--;
+	}
+	printf("fattoriale= %d",fat);
+}
